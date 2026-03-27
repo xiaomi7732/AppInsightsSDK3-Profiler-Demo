b275c36e7983cddf8146eb256615f04f848867f4
Saar Shen <saars@microsoft.com>
2026-03-27 16:03:34 -0700

    Support using private packages

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-using Azure.Monitor.OpenTelemetry.Exporter;
 using Azure.Monitor.OpenTelemetry.Profiler;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddApplicationInsightsTelemetry();
-builder.Services.AddOpenTelemetry().AddAzureMonitorProfiler();
+
+builder.Services
+  .AddApplicationInsightsTelemetry()
+  .AddAzureMonitorProfiler();
+
+//builder.Services.AddAzureMonitorProfiler(); // <--
+// builder.Services.AddOpenTelemetry().AddAzureMonitorProfiler(cft => );
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
